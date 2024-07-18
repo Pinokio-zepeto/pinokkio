db620f8d2522038f3f0702a350a96c37ece6c1b8
rlawnsdn <junoo1997@naver.com>
2024-07-18 12:54:44 +0900

    add cartPrice

--- frontend/src/components/kiosk/Cart.jsx
+++ frontend/src/components/kiosk/Cart.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import CartItem from "./CartItem";
+import CartTotal from "./CartTotal";
 
 const Ct = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   background: green;
   height: 500px;
   padding: 10px;
@@ -13,14 +15,17 @@ const Ct = styled.div`
 function Cart({ cartItems, setCartItems }) {
   return (
     <Ct>
-      {cartItems.map((item, index) => (
-        <CartItem
-          key={index}
-          item={item}
-          cartItems={cartItems}
-          setCartItems={setCartItems}
-        />
-      ))}
+      <div>
+        {cartItems.map((item, index) => (
+          <CartItem
+            key={index}
+            item={item}
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+          />
+        ))}
+      </div>
+      <CartTotal cartItems={cartItems} />
     </Ct>
   );
 }
