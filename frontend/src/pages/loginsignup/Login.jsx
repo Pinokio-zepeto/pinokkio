8e303ab2fff84e1d64b657124fc429ec6b03423d
sangmu <dltkdandl@naver.com>
2024-07-23 15:36:54 +0900

    feat: Login design

--- frontend/src/pages/loginsignup/Login.jsx
+++ frontend/src/pages/loginsignup/Login.jsx
@@ -1,8 +1,75 @@
 import React from "react";
-import "./Login.css";
-import Button from "../../components/common/Button";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import styled from "styled-components";
+
+import KioskNavbar from "../../components/common/KioskNavbar";
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f4f4f9;
+  padding: 20px;
+`;
+
+const LoginForm = styled.form`
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 100%;
+  max-width: 400px;
+  box-sizing: border-box;
+`;
+
+const Input = styled.input`
+  width: calc(100% - 20px);
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  font-size: 16px;
+  box-sizing: border-box;
+  color: #333;
+`;
+
+const StyledButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  background-color: rgb(102, 102, 255);
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #add8e6;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+
+  button {
+    width: 48%;
+    padding: 10px;
+    background-color: lightblue;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #add8e6;
+    }
+  }
+`;
 
 function Login() {
   const [id, setId] = useState("");
@@ -11,6 +78,7 @@ function Login() {
   const findPassword = () => {
     navigate("/findpassword1");
   };
+
   const signUp = () => {
     navigate("/signup");
   };
@@ -27,26 +95,28 @@ function Login() {
   };
 
   return (
-    <div className="Login">
-      <h2>로그인</h2>
-      <form id="login-form" onSubmit={handleLogin}>
-        <input
+    <LoginWrapper>
+      <KioskNavbar />
+      <LoginForm id="login-form" onSubmit={handleLogin}>
+        <Input
           type="text"
           className="Id"
-          placeholder="id"
+          placeholder="아이디"
           onChange={(e) => setId(e.target.value)}
-        ></input>
-        <input type="text" className="Password" placeholder="pw"></input>
-        <Button type="submit" text="로그인" />
-      </form>
-      <div>
-        <Button onClick={findPassword} text="비밀번호 찾기" />
-        <Button onClick={signUp} text="회원가입">
-          회원가입
-        </Button>
-      </div>
-    </div>
-  );
+        />
+        <Input
+          type="password"
+          className="Password"
+          placeholder="패스워드"
+        />
+        <StyledButton type="submit">로그인</StyledButton>
+      </LoginForm>
+      <ButtonWrapper>
+        <button onClick={findPassword}>비밀번호 찾기</button>
+        <button onClick={signUp}>회원가입</button>
+      </ButtonWrapper>
+    </LoginWrapper>
+  );  
 }
 
 export default Login;
